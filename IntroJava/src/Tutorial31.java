3203dd7624a7e4f2ebd92531be0515240fee6477
kylebaker <kylebaker@192.168.50.23>
2017-04-30 14:32:06 -0600

    initial commit

---
+++ IntroJava/src/Tutorial31.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+class Machine {
+	public String toString() {
+		return "I am a machine.";
+	}
+	
+	public void start() {
+		System.out.println("System started");
+	}
+}
+
+class Camera extends Machine {
+	
+}
+
+public class Tutorial31 {
+
+	public static void main(String[] args) {
+		ArrayList<Machine> list = new ArrayList<Machine>();
+		
+		list.add(new Machine());
+		list.add(new Machine());
+		
+		ArrayList<Camera> list2 = new ArrayList<Camera>();
+		
+		list2.add(new Camera());
+		list2.add(new Camera());
+		
+		showList(list2);
+	}
+	
+	public static void showList(ArrayList<? super Camera> list) {
+		for(Object value: list){
+			System.out.println(value);
+		}
+		
+		
+	}
+
+}
