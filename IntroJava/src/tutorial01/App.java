e57cb91bb1b155f390040701684f40bf2fd7cd41
kaker-faker <kjbker@gmail.com>
2017-09-02 10:06:17 -0600

    First 5 tutorials

--- IntroJava/src/tutorial01/App.java
+++ IntroJava/src/tutorial01/App.java
@@ -1,9 +1,7 @@
 package tutorial01;
 
 public class App {
-
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
-
 }
--- IntroJava/src/tutorial04/App.java
+++ IntroJava/src/tutorial04/App.java
@@ -3,8 +3,13 @@ package tutorial04;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int value = 0;
+		//boolean loop = value < 20;
+		
+		while(value < 10) {
+			System.out.println("Hello " + value);
+			value++;
+		}
 	}
 
 }
