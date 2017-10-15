aa3395566954c0fb7875beeadfa9211e66726654
kylebaker <kylebaker@fe80:0:0:0:a41e:5e72:6acc:a0f7%10>
2017-10-14 18:34:56 -0600

    Through tutorial 23

---
+++ IntroJava/src/tutorial22/Car.java
@@ -0,0 +1,12 @@
+package tutorial22;
+
+public class Car extends Machine {
+	@Override
+	public void start() {
+		System.out.println("Car started.");
+	}
+	
+	public void wipeWindShield() {
+		System.out.println("Wiping windshield");
+	}
+}
---
+++ IntroJava/src/tutorial23/Info.java
@@ -0,0 +1,5 @@
+package tutorial23;
+
+public interface Info {
+	public void showInfo();
+}
